dc20973e75175df441703d91f4374999c98da0d2
Paul Greenberg <greenpau@outlook.com>
2020-04-27 12:47:07 -0400

    add additional metric checks

--- metric.go
+++ metric.go
@@ -7,6 +7,19 @@ import (
 	"path/filepath"
 )
 
+var supportedOperations map[string]bool
+var supportedIndexSplit map[string]bool
+var supportedFuctions map[string]bool
+
+func init() {
+	supportedOperations = make(map[string]bool)
+	supportedIndexSplit = make(map[string]bool)
+	supportedFuctions = make(map[string]bool)
+	supportedOperations["GET"] = true
+	supportedIndexSplit["daily"] = true
+	supportedFuctions["_count"] = true
+}
+
 // Metric is a collection of attrbutes and parameters
 // for the creation and management of a metric.
 type Metric struct {
@@ -53,7 +66,47 @@ func NewMetricsFromFile(configFile string) ([]*Metric, error) {
 // that the fields conform to a standard set in this function.
 func (m *Metric) Valid() error {
 	if m.Name == "" {
-		return fmt.Errorf("attribute Name not found")
+		return fmt.Errorf("attribute Name not set in %v", *m)
 	}
+	if m.Category == "" {
+		return fmt.Errorf("attribute Category not set in %v", *m)
+	}
+	if m.Description == "" {
+		return fmt.Errorf("attribute Description not set in %v", *m)
+	}
+	if m.Operation == "" {
+		return fmt.Errorf("attribute Operation not set in %v", *m)
+	}
+	if m.BaseIndex == "" {
+		return fmt.Errorf("attribute BaseIndex not set in %v", *m)
+	}
+	if m.IndexSplit == "" {
+		return fmt.Errorf("attribute IndexSplit not set in %v", *m)
+	}
+	if m.Function == "" {
+		return fmt.Errorf("attribute Function not set in %v", *m)
+	}
+
+	if _, supported := supportedOperations[m.Operation]; !supported {
+		return fmt.Errorf(
+			"attribute Operation has unsupported value: %s, metric: %v",
+			m.Operation, *m,
+		)
+	}
+
+	if _, supported := supportedIndexSplit[m.IndexSplit]; !supported {
+		return fmt.Errorf(
+			"attribute IndexSplit has unsupported value: %s, metric: %v",
+			m.IndexSplit, *m,
+		)
+	}
+
+	if _, supported := supportedFuctions[m.Function]; !supported {
+		return fmt.Errorf(
+			"attribute Function has unsupported value: %s, metric: %v",
+			m.Function, *m,
+		)
+	}
+
 	return nil
 }
